0f8968cf734424437621ba44aa7610072dc1560e
samirraihan <samirraihan93@gmail.com>
2026-02-24 19:18:09 +0600

    Refactor routes in web.php for improved organization and clarity

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,37 @@
 <?php
 
+use App\Http\Controllers\JournalController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\SaleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
-Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('products', ProductController::class)
+        ->only(['index','store']);
+
+    Route::resource('sales', SaleController::class)
+        ->only(['index','store']);
+
+    Route::get('/reports', [ReportController::class,'index'])
+        ->name('reports.index');
+
+    Route::get('/journals', [JournalController::class,'index'])
+        ->name('journals.index');
+        
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
